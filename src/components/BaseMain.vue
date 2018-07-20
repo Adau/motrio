0fb5c477f78941eda9aa55f0168bd597958707d9
Adrien <adrien.dau@gmail.com>
2018-07-20 09:36:05 +0200

    Déplacer le composant CategoryList dans BaseMain
    Garder uniquement la logique du composant FamilyList

--- src/components/BaseMain.vue
+++ src/components/BaseMain.vue
@@ -2,19 +2,64 @@
   <main class="main">
     <h3>Recherche par <span class="text-primary">famille</span></h3>
 
-    <FamilyList/>
+    <BaseIconLoading v-if="isLoading"/>
+
+    <FamilyList
+      :families="families"
+      :selectedFamily="selectedFamily"
+      @clicked="setFamily"
+    />
+
+    <CategoryList
+      v-if="filteredCategories.length"
+      :categories="filteredCategories"
+    />
 
     <b-img src="../assets/img/footer-bg.jpg" fluid alt="footer" />
   </main>
 </template>
 
 <script>
+import axios from 'axios'
 import FamilyList from './FamilyList.vue'
+import CategoryList from './CategoryList.vue'
 
 export default {
   name: 'BaseMain',
   components: {
-    FamilyList
+    FamilyList,
+    CategoryList
+  },
+  data () {
+    return {
+      families: [],
+      categories: [],
+      selectedFamily: {},
+      isLoading: false
+    }
+  },
+  created () {
+    this.isLoading = true
+
+    axios.get('/search/categories')
+      .then(response => {
+        this.isLoading = false
+        this.families = response.data.families
+        this.categories = response.data.categories
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  },
+  computed: {
+    filteredCategories () {
+      return this.categories.filter(category => category.familyId == this.selectedFamily.familyId)
+    }
+  },
+  methods: {
+    setFamily(family) {
+      this.selectedFamily = family
+    }
   }
 }
 </script>
--- src/components/FamilyList.vue
+++ src/components/FamilyList.vue
@@ -1,64 +1,31 @@
 <template>
-  <div>
-    <BaseIconLoading v-if="isLoading"/>
-
-    <b-row class="family-list">
-      <FamilyListItem
-        v-for="family in families"
-        :key="family.familyId"
-        :family="family"
-        :isActive="family == selectedFamily"
-        @clicked="setFamily"
-      />
-    </b-row>
-
-    <CategoryList
-      v-if="filteredCategories.length"
-      :categories="filteredCategories"
+  <b-row class="family-list">
+    <FamilyListItem
+      v-for="family in families"
+      :key="family.familyId"
+      :family="family"
+      :isActive="family == selectedFamily"
+      @clicked="$emit('clicked', family)"
     />
-  </div>
+  </b-row>
 </template>
 
 <script>
-import axios from 'axios'
 import FamilyListItem from './FamilyListItem.vue'
-import CategoryList from './CategoryList.vue'
 
 export default {
   name: 'FamilyList',
   components: {
-    FamilyListItem,
-    CategoryList
+    FamilyListItem
   },
-  data () {
-    return {
-      selectedFamily: {},
-      families: [],
-      categories: [],
-      isLoading: false
-    }
-  },
-  computed: {
-    filteredCategories () {
-      return this.categories.filter(category => category.familyId == this.selectedFamily.familyId)
-    }
-  },
-  created () {
-    this.isLoading = true
-
-    axios.get('/search/categories')
-      .then(response => {
-        this.isLoading = false
-        this.families = response.data.families
-        this.categories = response.data.categories
-      })
-      .catch(error => {
-        console.error(error)
-      })
-  },
-  methods: {
-    setFamily(family) {
-      this.selectedFamily = family
+  props: {
+    families: {
+      type: Array,
+      required: true
+    },
+    selectedFamily: {
+      type: Object,
+      required: false
     }
   }
 }
